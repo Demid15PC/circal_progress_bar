ef5675b53d3eb2cf27ef74a737d4801913dc73e4
Demid <demidkuricyn@gmail.com>
2022-12-12 12:30:46 +0300

    second test

--- app/src/main/java/com/example/circal_progress_bar/MainActivity.kt
+++ app/src/main/java/com/example/circal_progress_bar/MainActivity.kt
@@ -10,25 +10,19 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.circal_progress_bar.ui.theme.Circal_progress_barTheme
 import com.example.circal_progress_bar.ui.theme.LightBlue
-import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,8 +33,8 @@ class MainActivity : ComponentActivity() {
                 contentAlignment = Alignment.Center
             ) {
                 CircularProgressBar(
-                    percentage = 0.3f,
-                    number = 300,
+                    percentage = 0.9f,
+                    number = 400,
                 )
             }
         }
@@ -54,24 +48,25 @@ fun CircularProgressBar(
     number: Int,
     radius: Dp = 50.dp,
     color: Color = LightBlue,
-    stroceWeight: Dp = 8.dp,
+    strokeWeight: Dp = 8.dp,
     animDuration: Int = 1000,
     animDelay: Int = 0,
 ) {
+
     var animationPlayed by remember {
         mutableStateOf(false)
-    } // это статус проигрывание анимации
+    }
+
     val animateFloatState = animateFloatAsState(
         targetValue = if (animationPlayed) percentage else 0f,
-        animationSpec = tween(
-            durationMillis = animDuration,
-            delayMillis = animDelay
-        )
+        animationSpec = tween(durationMillis = animDuration,delayMillis = animDelay)
     )
+
 
     LaunchedEffect(key1 = true) {
         animationPlayed = true
     }
+
 
     Box(
         contentAlignment = Alignment.Center,
@@ -83,7 +78,7 @@ fun CircularProgressBar(
                 -90f,
                 360 * animateFloatState.value,
                 useCenter = false,
-                style = Stroke(stroceWeight.toPx(), cap = StrokeCap.Round)
+                style = Stroke(strokeWeight.toPx(), cap = StrokeCap.Round)
             )
         }
         Text(
